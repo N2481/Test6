38fd42b5597d392f4c40bea478fe12485f052a13
JM <jose.morais@gruporumos.com>
2017-08-28 13:08:37 +0100

    xsasafsafd

--- app/src/main/java/com/jm/android/test6/MainActivity.java
+++ app/src/main/java/com/jm/android/test6/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int x = 1;
-
+        int x1 = 2;
     }
 }
